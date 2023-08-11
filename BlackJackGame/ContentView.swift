126ea1504b82489d551da5ab256f3a7d1f013c11
Phuc Hoang <phuchoang022002@gmail.com>
2023-08-11 11:06:50 +0700

    Init Game

--- BlackJackGame/ContentView.swift
+++ BlackJackGame/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var navigateToNextView = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Image("welcome-background")
+                    .resizable()
+                    .ignoresSafeArea()
+                VStack {
+                    Image("game-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(40)
+                    Text("Welcome to COSC2659 Black Jack Game")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .multilineTextAlignment(.center)
+                    Spacer()
+                    Button("Start Game") {
+                        navigateToNextView = true
+                    }
+                    .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+                    .foregroundColor(.white)
+                    .background(Color(UIColor(red: 0.01, green: 0.41, blue: 0.16, alpha: 1.00)))
+                    .cornerRadius(20)
+                    
+                    Spacer()
+                    
+                }
+            }
+        }.fullScreenCover(isPresented: $navigateToNextView) {
+            GameView()
         }
-        .padding()
     }
 }
 
---
+++ BlackJackGame/View/GameView.swift
@@ -0,0 +1,20 @@
+//
+//  GameView.swift
+//  BlackJackGame
+//
+//  Created by Phuc Hoang on 11/08/2023.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameView_Previews: PreviewProvider {
+    static var previews: some View {
+        GameView()
+    }
+}
