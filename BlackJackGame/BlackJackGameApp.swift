815146d3b996a0b6c29b940a183fd5f03a3aae4c
Phuc Hoang <phuchoang022002@gmail.com>
2023-08-11 10:36:09 +0700

    Initial Commit

---
+++ BlackJackGame/BlackJackGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackJackGameApp.swift
+//  BlackJackGame
+//
+//  Created by Phuc Hoang on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlackJackGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
